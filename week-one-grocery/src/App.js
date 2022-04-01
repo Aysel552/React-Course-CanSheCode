1de842461b8a348f915a80e4098d5b220e17743e
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-04-01 23:45:44 +0400

    added a button with onClick

--- week-one-grocery/src/App.js
+++ week-one-grocery/src/App.js
@@ -2,13 +2,28 @@
 import image from './grocery.jpeg';
 import './App.css';
 import ListOfProducts from './ListOfProducts';
+import ShoppingImage from './ShoppingImage';
 
 function App() {
+
+  //1st Option
+  function showAlert() {
+    alert("Clicked")
+  }
+
+  // 2nd option:
+  // const showAlert = () => {
+  //   alert("Clicked");
+  // }
+
   return (
     <div>
      
       <img src={image} className="App-logo" alt='Grocery' width="300px" />
       <ListOfProducts />
+      <ShoppingImage />
+      <button onClick={showAlert}>Submit</button>
+      
         
     </div>
   );
---
+++ week-one-grocery/src/ShoppingImage.js
@@ -0,0 +1,17 @@
+
+// import
+
+import image from './shopping-list.jpeg'
+
+//function
+
+function ShoppingImage() {
+    return (
+        <img src={image} className="App-logo" alt='Grocery' width="300px" />
+    )
+}
+
+
+//export
+
+export default ShoppingImage;
