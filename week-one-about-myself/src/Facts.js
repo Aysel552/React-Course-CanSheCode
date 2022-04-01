757c9bfc7d4d3f9c57782879bd2cf67c74ffa6bb
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-04-02 00:25:27 +0400

    created a folder about me using React components

---
+++ week-one-about-myself/src/Facts.js
@@ -0,0 +1,22 @@
+//import
+
+
+// function
+
+function Facts() {
+    return (
+        <div>
+            <h3>Facts about me</h3>
+            <ul>
+                <li>I love React</li>
+                <li>I love dancing</li>
+                <li>I like hiking</li>
+            </ul>
+        </div>
+    )
+}
+
+
+// export
+
+export default Facts;
---
+++ week-one-about-myself/src/Picture.js
@@ -0,0 +1,17 @@
+//import
+import image from './me.jpg'
+
+//function
+
+function Picture() {
+    return (
+        <div>
+            <img src={image} alt="Me" width="200px" height="300px"/>
+        </div>
+    )
+}
+
+
+//export
+
+export default Picture;
