030b34801890f9feb6ed5f3ecbd62cb4f57a982b
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-09-08 12:10:54 +0400

    practising api-s

--- week-four-api/week-four-exercise-five/src/App.js
+++ week-four-api/week-four-exercise-five/src/App.js
@@ -5,18 +5,18 @@ import './App.css';
 class Fox extends Component{
 
   state = {
-    image: null
+    : null
   }
   async componentDidMount() {
     const url = 'https://randomfox.ca/floof/';
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({image: data.image})
+    this.setState({url: data.url})
   }
   render() {
     return (
       <div>
-        <img src={this.state.image}  alt='Fox'/>
+        <img src={this.state.url} alt='Fox' width='400px'/>
       </div>
     )
   }
