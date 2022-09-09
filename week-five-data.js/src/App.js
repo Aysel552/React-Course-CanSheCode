1701fa66f8a7a8c4e946939581ca8a862bab93b8
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-09-09 15:19:51 +0400

    adding data.js file and practising map method

---
+++ week-five-data.js/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { data } from "./data";
+
+function App() {
+
+  const [toDo, setToDo] = useState(data);
+ 
+  return (
+    <div>
+      {toDo.map((thing => {
+        const { id, toDoData } = thing;
+        return (
+          <div key={id}>
+            <p>{id} {toDoData}</p>
+          </div>
+        )
+      }))}
+
+      <button onClick={() => setToDo([])}>Delete All</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ week-five-data.js/src/data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        id: 1,
+        toDoData: 'Wash my hair'
+    },
+
+    {
+        id: 2,
+        toDoData: 'JavaScript'
+    },
+
+    {
+        id: 3,
+        toDoData: 'React'
+    },
+
+    {
+        id: 4,
+        toDoData: 'English'
+    },
+
+    {
+        id: 5,
+        toDoData: 'Cook'
+    }
+]
