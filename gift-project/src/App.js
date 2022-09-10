aaa8237158cc43f45c29c1d0cc4a208cfa1cf8cd
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-09-10 13:26:04 +0400

    adding a gift project,using filter method

---
+++ gift-project/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { data } from './data';
+import './App.css';
+
+function App() {
+  const [gifts, setGifts] = useState(data);
+  const removeGift = (id) => {
+  
+
+    let newGifts = gifts.filter(gift => gift.id !== id);
+    setGifts(newGifts)
+    
+  }
+  return (
+    <div>
+      <div className='container'>
+        <h1>List of {gifts.length} gifts</h1>
+      </div>
+
+      {gifts.map((element => {
+        const { id, gift, image } = element;
+
+        return( <div key={id}>
+          <div className='container'>
+            <h2>{id} - {gift}</h2>
+          </div>
+
+          <div className='container'>
+            <img src={image} alt= 'Gifts' width='300px'/>
+          </div>
+
+          <div className='container'>
+            <button onClick={() => removeGift(id)}>Remove Item</button>
+
+          </div>
+
+        </div>)
+      }))}
+      <div className='container'>
+        <button onClick={() => setGifts([])}>delete all</button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
