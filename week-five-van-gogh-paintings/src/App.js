f25b877d2488f6f8d3cc4031805414ae27c4afab
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-09-12 22:51:49 +0400

    adding app top 5 paintings

--- week-five-van-gogh-paintings/src/App.js
+++ week-five-van-gogh-paintings/src/App.js
@@ -3,8 +3,65 @@ import { data } from './data';
 import './App.css';
 
 function App() {
+  const [paintings, setPaintings] = useState(0);
+  const [showMore, setShowMore] = useState(false); 
+  const { id, image, name, artist, description, create} = data[paintings];
+  
+
+  const previousPainting = () => {
+    setPaintings((painting => {
+      painting--;
+      if (painting < 0) {
+        return data.length - 1;
+      }
+      return painting
+    }))
+  }
+
+  const nextPainting = () => {
+    setPaintings((painting => {
+      painting++;
+      if (painting > data.length - 1) {
+        painting = 0
+      }
+      return painting
+    }))
+
+  }
+  
   return (
-    <div className="App">
+    <div>
+      <div className='container main'>
+      <h1>Top 5 famous paintings of Van Gogh</h1>
+      </div>
+
+      <div className='container'>
+        <h2>{id} - {name}</h2>
+      </div>
+
+      <div className='container'>
+        <img src={image} alt='Paintings' width='370px' height='300px'/>
+      </div>
+
+
+      <div className='container'>
+        <h3>{artist}</h3>
+      </div>
+
+      <div className='container'>
+        <h3>{create}</h3>
+      </div>
+    
+      <div className='container'>
+        <p>{showMore ? description : description.substring(0, 220) + '.....'}
+          <button className='showMoreButton' onClick={() => setShowMore(!showMore)}>{showMore ? 'Show Less' : 'Show More'}</button>
+        </p>
+      </div>
+
+      <div className='container btn'>
+        <button onClick ={previousPainting}>Previous</button>
+        <button onClick={nextPainting}>Next</button>
+      </div>
       
     </div>
   );
--- week-five-van-gogh-paintings/src/data.js
+++ week-five-van-gogh-paintings/src/data.js
@@ -1,58 +1,48 @@
 export const data = [
     {
         id: 1,
-        image: 'https://artsandculture.google.com/asset/the-starry-night/bgEuwDxel93-Pg?utm_source=google&utm_medium=kp&hl=en-GB&avm=2',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1200px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
         name: 'The Starry Night',
         artist: 'Vincent van Gogh',
-        description: 'The Starry Night is an oil-on-canvas painting by the Dutch Post-Impressionist painter Vincent van Gogh. Painted in June 1889, it depicts the view from the east-facing window of his asylum room at Saint-Rémy-de-Provence, just before sunrise, with the addition of an imaginary village.',
+        description: "The Starry Night is an oil-on-canvas painting by the Dutch Post-Impressionist painter Vincent van Gogh. Painted in June 1889, it depicts the view from the east-facing window of his asylum room at Saint-Rémy-de-Provence, just before sunrise, with the addition of an imaginary village.",
         create: 'June 1889',
-        genre: 'Landscape painting',
-        periods: 'Post-Impressionism, Modern Art'
     },
 
     {
         id: 2,
-        image: '',
+        image: 'https://iiif.micr.io/rAERt/full/1280,/0/default.jpg',
         name: 'Almond Blossoms',
         artist: 'Vincent van Gogh',
-        description: 'Almond Blossoms is a group of several paintings made in 1888 and 1890 by Vincent van Gogh in Arles and Saint-Rémy, southern France of blossoming almond trees. Flowering trees were special to van Gogh. They represented awakening and hope. He enjoyed them aesthetically and found joy in painting flowering trees.',
+        description: "Almond Blossoms is a group of several paintings made in 1888 and 1890 by Vincent van Gogh in Arles and Saint-Rémy, southern France of blossoming almond trees. Flowering trees were special to van Gogh. They represented awakening and hope. He enjoyed them aesthetically and found joy in painting flowering trees.",
         create: '1890',
-        genre: 'Almond Blossoms',
-        periods: 'Post-Impressionism, Modern Art'
     },
 
 {
         id: 3,
-        image: '',
-        name: 'Vase with Fifteen Sunflowers',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Vincent_Van_Gogh_-_Wheatfield_with_Crows.jpg/1200px-Vincent_Van_Gogh_-_Wheatfield_with_Crows.jpg',
+        name: 'Wheatfield with Crows',
         artist: 'Vincent van Gogh',
-        description: 'Still Life: Vase with Fifteen Sunflowers Oil Painting Reproduction, hand-painted in oil on canvas. 3 Reviews | Add review. This breathtaking art piece is from the Arles Sunflowers collection.',
-        create: 'August 1888',
-        genre: 'Still life',
-        periods: 'Post-Impressionism, Modern Art'
+        description: "Wheatfield with Crows is a July 1890 painting by Vincent van Gogh. It has been cited by several critics as one of his greatest works. It is commonly stated that this was van Gogh's final painting. However, art historians are uncertain as to which painting was van Gogh's last, as no clear historical records exist.",
+        create: '1890',
     },
 
     {
         id: 4,
-        image: '',
+        image: 'https://cdn.shopify.com/s/files/1/0518/5958/7227/products/ppal_80cc0973-b3f2-4dca-b3b1-721842b0623d_1080x.png?v=1631743386',
         name: 'Irises',
         artist: 'Vincent van Gogh',
-        description: 'Irises is one of several paintings of irises by the Dutch artist Vincent van Gogh, and one of a series of paintings he made at the Saint Paul-de-Mausole asylum in Saint-Rémy-de-Provence, France, in the last year before his death in 1890.',
+        description: "Irises is one of several paintings of irises by the Dutch artist Vincent van Gogh, and one of a series of paintings he made at the Saint Paul-de-Mausole asylum in Saint-Rémy-de-Provence, France, in the last year before his death in 1890.",
         create: 'August 1888',
-        genre: 'Still life',
-        periods: 'Post-Impressionism, Modern Art'
     },
 
      {
         id: 5,
-        image: '',
-        name: 'Irises',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Red_vineyards.jpg/440px-Red_vineyards.jpg',
+        name: 'The Red Vineyard',
         artist: 'Vincent van Gogh',
-        description: 'Irises is one of several paintings of irises by the Dutch artist Vincent van Gogh, and one of a series of paintings he made at the Saint Paul-de-Mausole asylum in Saint-Rémy-de-Provence, France, in the last year before his death in 1890.',
-        create: 'August 1888',
-        genre: 'Still life',
-        periods: 'Post-Impressionism, Modern Art'
-    },
+        description: "The Red Vineyards near Arles is an oil painting by the Dutch painter Vincent van Gogh, executed on a privately primed Toile de 30 piece of burlap in early November 1888. It depicts workers in a vineyard, and it is the only painting known by name that van Gogh sold in his lifetime.",
+        create: '1888',
+    }
 
 
 
