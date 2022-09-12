6a246b5b81ce74dbb951f6c7bbf61bd76c18cc27
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-09-12 20:16:10 +0400

    adding data.js file

---
+++ week-five-van-gogh-paintings/src/data.js
@@ -0,0 +1,76 @@
+export const data = [
+    {
+        id: 1,
+        image: 'https://artsandculture.google.com/asset/the-starry-night/bgEuwDxel93-Pg?utm_source=google&utm_medium=kp&hl=en-GB&avm=2',
+        name: 'The Starry Night',
+        artist: 'Vincent van Gogh',
+        description: 'The Starry Night is an oil-on-canvas painting by the Dutch Post-Impressionist painter Vincent van Gogh. Painted in June 1889, it depicts the view from the east-facing window of his asylum room at Saint-Rémy-de-Provence, just before sunrise, with the addition of an imaginary village.',
+        create: 'June 1889',
+        genre: 'Landscape painting',
+        periods: 'Post-Impressionism, Modern Art'
+    },
+
+    {
+        id: 2,
+        image: '',
+        name: 'Almond Blossoms',
+        artist: 'Vincent van Gogh',
+        description: 'Almond Blossoms is a group of several paintings made in 1888 and 1890 by Vincent van Gogh in Arles and Saint-Rémy, southern France of blossoming almond trees. Flowering trees were special to van Gogh. They represented awakening and hope. He enjoyed them aesthetically and found joy in painting flowering trees.',
+        create: '1890',
+        genre: 'Almond Blossoms',
+        periods: 'Post-Impressionism, Modern Art'
+    },
+
+{
+        id: 3,
+        image: '',
+        name: 'Vase with Fifteen Sunflowers',
+        artist: 'Vincent van Gogh',
+        description: 'Still Life: Vase with Fifteen Sunflowers Oil Painting Reproduction, hand-painted in oil on canvas. 3 Reviews | Add review. This breathtaking art piece is from the Arles Sunflowers collection.',
+        create: 'August 1888',
+        genre: 'Still life',
+        periods: 'Post-Impressionism, Modern Art'
+    },
+
+    {
+        id: 4,
+        image: '',
+        name: 'Irises',
+        artist: 'Vincent van Gogh',
+        description: 'Irises is one of several paintings of irises by the Dutch artist Vincent van Gogh, and one of a series of paintings he made at the Saint Paul-de-Mausole asylum in Saint-Rémy-de-Provence, France, in the last year before his death in 1890.',
+        create: 'August 1888',
+        genre: 'Still life',
+        periods: 'Post-Impressionism, Modern Art'
+    },
+
+     {
+        id: 5,
+        image: '',
+        name: 'Irises',
+        artist: 'Vincent van Gogh',
+        description: 'Irises is one of several paintings of irises by the Dutch artist Vincent van Gogh, and one of a series of paintings he made at the Saint Paul-de-Mausole asylum in Saint-Rémy-de-Provence, France, in the last year before his death in 1890.',
+        create: 'August 1888',
+        genre: 'Still life',
+        periods: 'Post-Impressionism, Modern Art'
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
