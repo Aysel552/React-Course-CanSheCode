c9badcd112e6259d4b0d5c71804cb510dfab8e31
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-09-14 18:49:45 +0400

    adding a project with react-router

--- week-six-online-shopping-app/src/App.js
+++ week-six-online-shopping-app/src/App.js
@@ -1,28 +1,36 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import { useState } from 'react';
 import { data } from './data';
 import './App.css';
 import Clothes from './Clothes';
 import Buttons from './Buttons';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
 
 function App() {
-  const [clothes, setClothes] = useState(data);
-  const chosenClothes = (searchTerm) => {
-    const newClothes = data.filter(element => element.searchTerm === searchTerm);
-    setClothes(newClothes)
-
-    
-    
-  }
   return (
-    <div >
-      <div className='cont'>
-        <h2 className='back'>Free Standard Shipping</h2>
-      </div>
-      <Buttons filteredClothes={chosenClothes} />
-      <Clothes itemsForSale={clothes} />
-      
-    </div>
-  );
+    <Router>
+      <nav>
+        <Link to='/' className='link'>Shop</Link>
+        <Link to='/about' className='link'>About</Link>
+        <Link to='/contact' className='link'>Contact</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About/> } />
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
