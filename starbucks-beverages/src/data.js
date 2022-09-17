f5afa5a8d5744a850c47bebda77e0de016ab9aa1
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-09-17 12:45:54 +0400

    adding details to data.js file

--- starbucks-beverages/src/data.js
+++ starbucks-beverages/src/data.js
@@ -14,7 +14,7 @@ export const data = [
         searchTerm: 'dress',
         productName: 'Cottagecore Dress',
         image: 'https://i.pinimg.com/564x/29/71/53/297153cb0fb8047d9ea8eef0e7311dd2.jpg',
-        price: 190.00
+        price: 195.00
     },
     
     {
@@ -22,7 +22,7 @@ export const data = [
         searchTerm: 'dress',
         productName: 'Fashion Dress',
         image: 'https://i.pinimg.com/564x/32/e4/1b/32e41b13e838c7d543337458474c8a3e.jpg',
-        price: 190.00
+        price: 200.00
     },
     
     {
@@ -30,7 +30,7 @@ export const data = [
         searchTerm: 'dress',
         productName: 'Dress with Long Sleeves',
         image: 'https://i.pinimg.com/564x/1e/b9/b7/1eb9b780c0362abeb580bfff1271aff2.jpg',
-        price: 190.00
+        price: 324.00
     },
     
     {
@@ -38,7 +38,7 @@ export const data = [
         searchTerm: 'dress',
         productName: 'Mock-neck Button detail Lantern Sleeve Dress',
         image: 'https://i.pinimg.com/564x/03/95/82/039582785b91c84254fd61d4aa59b8d1.jpg',
-        price: 190.00
+        price: 500.00
     },
     
     {
@@ -46,7 +46,7 @@ export const data = [
         searchTerm: 'dress',
         productName: 'Ruched Sleeve Wide Waistband Buttoned Dress',
         image: 'https://i.pinimg.com/564x/56/70/40/567040d5b7a45d46722e0b4ea2e9313c.jpg',
-        price: 190.00
+        price: 210.00
     },
     
     {
@@ -54,7 +54,7 @@ export const data = [
         searchTerm: 'dress',
         productName: 'Midi Dress',
         image: 'https://i.pinimg.com/474x/69/34/90/69349055e21b286090f0b8ca9332da70.jpg',
-        price: 190.00
+        price: 250.00
     },
 
     {
@@ -73,7 +73,7 @@ export const data = [
         searchTerm: 'jeans',
         productName: 'Flared High Waisted Jeans',
         image: 'https://i.pinimg.com/564x/70/f8/22/70f822c377227b7bb2fac501b1788129.jpg',
-        price: 115.00
+        price: 217.00
     },
 
     
@@ -82,7 +82,7 @@ export const data = [
         searchTerm: 'jeans',
         productName: 'Light Blue Jeans',
         image: 'https://i.pinimg.com/564x/5e/d5/6f/5ed56fe41d4093fff8eaf6f918c10af5.jpg',
-        price: 115.00
+        price: 320.00
     },
     
     {
@@ -98,7 +98,7 @@ export const data = [
         searchTerm: 'jeans',
         productName: 'Light Blue Mom Jeans',
         image: 'https://i.pinimg.com/564x/e9/8c/a7/e98ca7a934aeab29b2eeb790b42506e0.jpg',
-        price: 115.00
+        price: 129.00
     },
     
     {
@@ -106,7 +106,7 @@ export const data = [
         searchTerm: 'jeans',
         productName: 'Dark Grey Jeans',
         image: 'https://i.pinimg.com/564x/dc/1b/6e/dc1b6e31a47410f1f641aec632d23a21.jpg',
-        price: 115.00
+        price: 138.00
     },
 
     {
@@ -114,7 +114,7 @@ export const data = [
         searchTerm: 'jeans',
         productName: 'White Jeans',
         image: 'https://i.pinimg.com/474x/7b/6b/b5/7b6bb5704dd8958e0bf90ae34dff3c0e.jpg',
-        price: 115.00
+        price: 165.00
     },
 
     {
@@ -122,7 +122,7 @@ export const data = [
         searchTerm: 'jeans',
         productName: 'Wide Jeans',
         image: 'https://i.pinimg.com/564x/7d/37/17/7d3717723f5d031956deac27fc333aab.jpg',
-        price: 115.00
+        price: 185.00
     },
 
 
@@ -131,7 +131,7 @@ export const data = [
         searchTerm: 'jeans',
         productName: 'Wide Jeans',
         image: 'https://i.pinimg.com/474x/4c/32/27/4c32275b8d34953bf067542ca54d9ce2.jpg',
-        price: 115.00
+        price: 195.00
     },
     // Jackets
     
@@ -140,7 +140,7 @@ export const data = [
         searchTerm: 'jacket',
         productName: 'Knitted Jacket',
         image: 'https://i.pinimg.com/474x/b1/e6/f5/b1e6f5090ebf027112743a60574b7d36.jpg',
-        price: 160.00
+        price: 205.00
     },
 
     {
@@ -148,7 +148,7 @@ export const data = [
         searchTerm: 'jacket',
         productName: 'Knitted Cardigan',
         image: 'https://i.pinimg.com/474x/86/fd/b0/86fdb00d7eb5114dc95c029f4db095d7.jpg',
-        price: 160.00
+        price: 310.00
     },
 
     {
@@ -156,7 +156,7 @@ export const data = [
         searchTerm: 'jacket',
         productName: 'Short Knitted Jacket',
         image: 'https://i.pinimg.com/474x/79/ae/1c/79ae1c558f8a01de34c5d9ab69e4a4c1.jpg',
-        price: 160.00
+        price: 260.00
     },
 
     {
@@ -164,7 +164,7 @@ export const data = [
         searchTerm: 'jacket',
         productName: 'Blue Knitted Jacket',
         image: 'https://i.pinimg.com/474x/36/38/3c/36383cf89a2f33c41f3d53e9cb783093.jpg',
-        price: 160.00
+        price: 230.00
     },
     
     {
@@ -172,7 +172,7 @@ export const data = [
         searchTerm: 'jacket',
         productName: 'Camel Color Knitted Jacket',
         image: 'https://i.pinimg.com/474x/09/3b/b5/093bb56180ba3efb93312e2e75e8e039.jpg',
-        price: 160.00
+        price: 250.00
     },
     
     {
@@ -180,7 +180,7 @@ export const data = [
         searchTerm: 'jacket',
         productName: 'Camel Color Knitted Cardigan ',
         image: 'https://i.pinimg.com/474x/71/2c/ed/712cede80113a5c84374bb61da01efd2.jpg',
-        price: 160.00
+        price: 230.00
     },
     
     {
@@ -188,7 +188,7 @@ export const data = [
         searchTerm: 'jacket',
         productName: 'Wool Cardigan',
         image: 'https://i.pinimg.com/474x/da/78/5d/da785de93ebed23a8bbe3b3665008833.jpg',
-        price: 160.00
+        price: 500.00
     },
     
     {
@@ -196,7 +196,7 @@ export const data = [
         searchTerm: 'jacket',
         productName: 'Chunky Cable Cardigan',
         image: 'https://i.pinimg.com/564x/a2/7f/28/a27f289c9c49b51634bf1f0d4869ceba.jpg',
-        price: 160.00
+        price: 572.00
     },
     //  Skirts
     {
@@ -204,7 +204,7 @@ export const data = [
         searchTerm: 'skirt',
         productName: 'Camel Color Silk Skirt',
         image: 'https://i.pinimg.com/474x/77/03/25/770325239998dc736c60c7e7c98b5e7a.jpg',
-        price: 150.00
+        price: 563.00
     },
 
     {
@@ -212,7 +212,7 @@ export const data = [
         searchTerm: 'skirt',
         productName: 'Mini Skirt',
         image: 'https://i.pinimg.com/474x/25/71/b7/2571b7ab34b965e19e8c37cec2b7800a.jpg',
-        price: 150.00
+        price: 432.00
     },
 
     {
@@ -220,7 +220,7 @@ export const data = [
         searchTerm: 'skirt',
         productName: 'Mini Skirt',
         image: 'https://i.pinimg.com/474x/39/a6/27/39a6276dbdb62a03e30404165ffddf67.jpg',
-        price: 150.00
+        price: 310.00
     },
 
     {
@@ -228,7 +228,7 @@ export const data = [
         searchTerm: 'skirt',
         productName: 'Mini Skirt',
         image: 'https://i.pinimg.com/474x/4f/66/63/4f666315220fe0b1b1bd966b3b088b75.jpg',
-        price: 150.00
+        price: 200.00
     },
 
     
@@ -237,7 +237,7 @@ export const data = [
         searchTerm: 'skirt',
         productName: 'Silk Skirt',
         image: 'https://i.pinimg.com/564x/78/a1/bd/78a1bd14a07afd794261656b56aec00b.jpg',
-        price: 150.00
+        price: 319.00
     },
     
     {
@@ -245,7 +245,7 @@ export const data = [
         searchTerm: 'skirt',
         productName: 'Silk Skirt',
         image: 'https://i.pinimg.com/564x/18/a6/b1/18a6b10bfa3b306894a78590c951c00d.jpg',
-        price: 150.00
+        price: 390.00
     },
     
     {
@@ -253,7 +253,7 @@ export const data = [
         searchTerm: 'skirt',
         productName: 'Silk Skirt',
         image: 'https://i.pinimg.com/474x/50/47/3d/50473d7350459fe4729cfaec0706c008.jpg',
-        price: 150.00
+        price: 235.00
     },
     
     {
@@ -261,7 +261,7 @@ export const data = [
         searchTerm: 'skirt',
         productName: 'High Waist Skirt',
         image: 'https://i.pinimg.com/564x/c1/5a/94/c15a942caec73ceabdd2fbfc7baf7776.jpg',
-        price: 150.00
+        price: 198.00
     },
 
     // Coat
@@ -270,84 +270,124 @@ export const data = [
         searchTerm: 'coat',
         productName: 'White Color Coat',
         image: 'https://i.pinimg.com/474x/69/e9/3c/69e93c5c9698d35e16ff48b2599e1a5f.jpg',
-        price: 170.00
+        price: 400.00
     },
 
     {
         id: 34,
         searchTerm: 'coat',
-        productName: 'Burberry Trench Coat',
-        image: 'https://i.pinimg.com/474x/46/d4/7d/46d47da6c4fb8cd9bafdf157377bb6b0.jpg',
-        price: 170.00
+        productName: 'Tweed Coat',
+        image: 'https://i.pinimg.com/474x/2e/a5/05/2ea5055df8bb31ca716dc191f4d18e81.jpg',
+        price: 450.00
     },
-    
+
     {
         id: 35,
         searchTerm: 'coat',
+        productName: 'Pacific Coat',
+        image: 'https://i.pinimg.com/474x/db/c4/01/dbc40144cd96ac7f6dd55ef7cbe79c49.jpg',
+        price: 435.00
+    },
+
+    {
+        id: 36,
+        searchTerm: 'coat',
+        productName: 'Camel Color Coat',
+        image: 'https://i.pinimg.com/474x/ae/0d/f7/ae0df765f8d17bd5e50849db8b56b55a.jpg',
+        price: 420.00
+    },
+
+    {
+        id: 37,
+        searchTerm: 'coat',
+        productName: 'Burberry Trench Coat',
+        image: 'https://i.pinimg.com/474x/46/d4/7d/46d47da6c4fb8cd9bafdf157377bb6b0.jpg',
+        price: 520.00
+    },
+    
+    {
+        id: 38,
+        searchTerm: 'coat',
         productName: 'Grey Coat',
         image: 'https://i.pinimg.com/564x/89/0a/6c/890a6c6d89c2084d81d08a4d911d40d6.jpg',
-        price: 170.00
+        price: 325.00
     },
     
         {
-        id: 36,
+        id: 39,
         searchTerm: 'coat',
         productName: 'Winter Coat',
         image: 'https://i.pinimg.com/474x/97/6a/a7/976aa712b28094d1e126823271a6b878.jpg',
-        price: 170.00
+        price: 420.00
     },
         {
-        id: 37,
+        id: 40,
         searchTerm: 'coat',
         productName: 'Winter Coat',
         image: 'https://i.pinimg.com/474x/c8/51/f0/c851f08808a73f742371119464dc53b2.jpg',
-        price: 170.00
+        price: 320.00
     },
 
     // Shoes
         {
-        id: 38,
+        id: 41,
         searchTerm: 'shoes',
         productName: 'Chanel Shoes',
         image: 'https://i.pinimg.com/474x/31/59/a2/3159a2f00556c4dcbc565b5106e4706d.jpg',
-        price: 200.00
+        price: 300.00
     },
         
-        {
-        id: 39,
-        searchTerm: 'shoes',
-        productName: 'High Heels Sandals',
-        image: 'https://i.pinimg.com/474x/f6/71/18/f67118ac3e9230f8aeb70a7c0dea2dac.jpg',
-        price: 150.00
-    },
-        {
-        id: 40,
-        searchTerm: 'shoes',
-        productName: 'Camel Color Shoes',
-        image: 'https://i.pinimg.com/474x/0b/47/fb/0b47fbd5de0926ca16ed461a28d4caf0.jpg',
-        price: 145.00
-    },
-        
-    {
-        id: 41,
-        searchTerm: 'shoes',
-        productName: 'Boots',
-        image: 'https://i.pinimg.com/474x/e1/80/5f/e1805f15e5df2421322e5517b0b53179.jpg',
-        price: 130.00
-    },
         {
         id: 42,
         searchTerm: 'shoes',
+        productName: 'High Heels Sandals',
+        image: 'https://i.pinimg.com/474x/f6/71/18/f67118ac3e9230f8aeb70a7c0dea2dac.jpg',
+        price: 450.00
+    },
+        {
+        id: 43,
+        searchTerm: 'shoes',
+        productName: 'Camel Color Shoes',
+        image: 'https://i.pinimg.com/474x/0b/47/fb/0b47fbd5de0926ca16ed461a28d4caf0.jpg',
+        price: 432.00
+    },
+        
+    {
+        id: 44,
+        searchTerm: 'shoes',
+        productName: 'Boots',
+        image: 'https://i.pinimg.com/474x/e1/80/5f/e1805f15e5df2421322e5517b0b53179.jpg',
+        price: 542.00
+    },
+        {
+        id: 45,
+        searchTerm: 'shoes',
         productName: 'Boots',
         image: 'https://i.pinimg.com/474x/5d/48/0e/5d480efd7b2e513f1d5d1a5a87360589.jpg',
-        price: 120.00
+        price: 432.00
     },
 
     {
-        id: 43,
+        id: 46,
         searchTerm: 'shoes',
         productName: 'Sneakers',
         image: 'https://i.pinimg.com/474x/55/d2/9d/55d29d932a7f8b9d37e7c14aac4d3f81.jpg',
-        price: 120.00
+        price: 470.00
+    },
+
+    {
+        id: 47,
+        searchTerm: 'shoes',
+        productName: 'High Heels Shoes',
+        image: 'https://i.pinimg.com/474x/52/4c/16/524c16ddfe721526abb1804198b10b9a.jpg',
+        price: 480.00
+    },
+
+    {
+        id: 48,
+        searchTerm: 'shoes',
+        productName: 'High Heels Shoes',
+        image: 'https://i.pinimg.com/474x/79/0f/54/790f547f5c60c7e75c930dbb4b193f43.jpg',
+        price: 497.00
     }
 ]
