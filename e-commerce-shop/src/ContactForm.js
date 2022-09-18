8db6f2a53164f097d09f3c00c255bc5d23b3f490
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-09-18 22:22:16 +0400

    working on form section

--- e-commerce-shop/src/ContactForm.js
+++ e-commerce-shop/src/ContactForm.js
@@ -4,7 +4,46 @@ import "./App.css"
 
 const FORM_ENDPOINT = "";
 
+const ContactForm = () => {
+    const [submitted,setSubmitted] = useState(false);
+    const handleSubmit = () =>{
+        setTimeout(() => {
+            setSubmitted(true);
+        },100);
+    };
+    if (submitted){
+        return(
+            <div>
+            <h2>Thank you!</h2>
+            <div>We'll be in touch soon</div>
+            </div>
+        );
+    }
 
+    return(
+        <form
+        action={FORM_ENDPOINT}
+        onSubmit={handleSubmit}
+        method="POST"
+        target="_blank">
+            <div className="container">
+                <input type="text" placeholder="Your name" name="name" required/>
+            </div>
+
+            <div className="container">
+                <input type="email" placeholder="Email" name="email" required/>
+            </div>
+
+            <div className="container">
+                <textarea placeholder="Your message" name="message" required/>
+            </div>
+
+            <div className="container">
+                <button className="sendMessage" type="submit">Send a message</button>
+            </div>
+        </form>
+    )
+}
 
 
 export default ContactForm;
