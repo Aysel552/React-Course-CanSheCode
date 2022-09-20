ec97e919bd23cda433c2132ff514e556398f77c7
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-09-20 19:25:15 +0400

    created a form

--- e-commerce-shop/src/ContactForm.js
+++ e-commerce-shop/src/ContactForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 
     function ContactForm() {
         const [formStatus,setFormStatus] = useState('Send');
@@ -18,26 +19,40 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
         }
     
     return(
-        <div>
-        <div className="container mt-5">
-            <h2 className="mb-3" >React Contact Form Example</h2>
+        <div className="main">
+        <div className="container">
+            <h2 className=" heading mb-5">Contact Form</h2>
             </div>
             <form onSubmit={onSubmit}>
-                <div className="mb-3">
+                <div className="container mb-4 w-50">
+                    <div className="container">
                 <label className="form-label" htmlFor="name">Name</label>
+                <div className="container mx-3">
                 <input className="form-control" type="text" id="name" required/>
                 </div>
+                </div>
+                </div>
 
-                <div className="mb-3">
+                <div className="container mb-4 w-50">
+                <div className="container">
                 <label className="form-label" htmlFor="email">Email</label>
+                <div className="container mx-3">
                 <input className="form-control" type="email" id="name" required/>
                 </div>
+                </div>
+                </div>
 
-                <div className="mb-3">
+                <div className="container mb-4 w-50">
+                    <div className="container">
                 <label className="form-label" htmlFor="message">Message</label>
+                <div className="container w-100">
                 <textarea className="form-control" id="message" required/>
                 </div>
+                </div>
+                </div>
+                <div className="container">
                 <button className="btn btn-danger" type="submit">{formStatus}</button>
+                </div>
             </form>
         </div>
     )
